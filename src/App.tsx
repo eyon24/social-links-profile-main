2328c91e2d3b48e419fd9f66afc6d1e060b9aad1
Eric <ericyon24@gmail.com>
2026-02-07 21:43:00 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div id="root" className="main">
+      <div className="card">
+        <img src="" alt="" />
+        <h1>Jessica Randall</h1>
+        <span>location</span>
+        <p>"Front-end developer and avid reader."</p>
+        <ul>
+          <li>GitHub</li>
+          <li>Frontend Mentor</li>
+          <li>LinkedIn</li>
+          <li>Twitter</li>
+          <li>Instagram</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;

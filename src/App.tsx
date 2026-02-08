a8c90d331be1ed0ad853c4781c1d67ebb3dffc2c
Eric <ericyon24@gmail.com>
2026-02-07 22:28:50 -0600

    mobile complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import "./App.css";
+import portrait from "./assets/avatar-jessica.jpeg";
 
 function App() {
   return (
     <div id="root" className="main">
       <div className="card">
-        <img src="" alt="" />
-        <h1>Jessica Randall</h1>
-        <span>location</span>
+        <img src={portrait} alt="Jessica Randall" />
+        <div className="info">
+          <h1>Jessica Randall</h1>
+          <span>London, United Kingdom</span>
+        </div>
         <p>"Front-end developer and avid reader."</p>
-        <ul>
-          <li>GitHub</li>
-          <li>Frontend Mentor</li>
-          <li>LinkedIn</li>
-          <li>Twitter</li>
-          <li>Instagram</li>
-        </ul>
+
+        <div className="links">
+          <a href="https://github.com/">GitHub</a>
+          <a href="https://www.frontendmentor.io/">Frontend Mentor</a>
+          <a href="https://www.linkedin.com/">LinkedIn</a>
+          <a href="https://www.x.com">X</a>
+          <a href="https://www.instagram.com/">Instagram</a>
+        </div>
       </div>
     </div>
   );
